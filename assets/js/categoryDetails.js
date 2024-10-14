821f12e6fcdb01a06bd855b9224915e8de49cc61
tasneemjber <149652509+tasneemjber@users.noreply.github.com>
2024-10-14 15:26:13 +0300

    Add files via upload

---
+++ assets/js/categoryDetails.js
@@ -0,0 +1,24 @@
+const getProducts = async () => {
+    const params = new URLSearchParams(window.location.search);
+    const category = params.get('category');
+    const { data } = await axios.get(`https://dummyjson.com/products/category/${category}`);
+    return data;
+  };
+  
+  const displayProducts = async () => {
+    const data = await getProducts();
+    const result = data.products.map((product) => {
+      return `
+        <div class='product'>
+          <img src="${product.thumbnail}" alt="${product.description}" />
+          <h3>${product.title}</h3>
+          <span>${product.price}</span>
+        </div>
+      `;
+    }).join('');
+  
+    document.querySelector(".products .row").innerHTML = result;
+  };
+  
+  displayProducts();
+  
